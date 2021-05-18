8adfef347c255ca6968571df6092fcb4615d235e
csse4011 <csse4011@csse4011.csse4011>
2021-05-18 19:42:31 +1000

    changed file location

--- apps/mobile/src/main.c
+++ apps/mobile/src/main.c
@@ -40,6 +40,100 @@ static struct bt_uuid_16 ble_read_uuid = BT_UUID_INIT_16(0xf1de);
 int32_t ble;
 bool static_node = false;
 
+static void fetch_and_display(const struct shell *shell, const struct device *sensor)
+{
+	static unsigned int count;
+	struct sensor_value accel[3];
+	const char *overrun = "";
+	int rc = sensor_sample_fetch(sensor);
+
+	++count;
+	if (rc == -EBADMSG) {
+		/* Sample overrun.  Ignore in polled mode. */
+		if (IS_ENABLED(CONFIG_LIS2DH_TRIGGER)) {
+			overrun = "[OVERRUN] ";
+		}
+		rc = 0;
+	}
+	if (rc == 0) {
+		rc = sensor_channel_get(sensor,
+					SENSOR_CHAN_ACCEL_XYZ,
+					accel);
+	}
+	if (rc < 0) {
+		printk("ERROR: Update failed: %d\n", rc);
+	} else {
+		printk("#%u @ %u ms: %sx %f , y %f , z %f\n",
+		       count, k_uptime_get_32(), overrun,
+		       sensor_value_to_double(&accel[0]),
+		       sensor_value_to_double(&accel[1]),
+		       sensor_value_to_double(&accel[2]));
+	}
+}
+
+#ifdef CONFIG_LIS2DH_TRIGGER
+static void trigger_handler(const struct device *dev,
+			    struct sensor_trigger *trig)
+{
+	fetch_and_display(dev);
+}
+#endif
+
+void main(const struct shell *shell, size_t argc, char **argv)
+{
+    sensor = device_get_binding(DT_LABEL(DT_INST(0, st_lis2dh)));
+	if (sensor == NULL) {
+		printk("Could not get %s device\n",
+		       DT_LABEL(DT_INST(0, st_lis2dh)));
+		return;
+	}
+
+#if CONFIG_LIS2DH_TRIGGER
+	{
+		struct sensor_trigger trig;
+		int rc;
+
+		trig.type = SENSOR_TRIG_DATA_READY;
+		trig.chan = SENSOR_CHAN_ACCEL_XYZ;
+
+		if (IS_ENABLED(CONFIG_LIS2DH_ODR_RUNTIME)) {
+			struct sensor_value odr = {
+				.val1 = 1,
+			};
+
+			rc = sensor_attr_set(sensor, trig.chan,
+					     SENSOR_ATTR_SAMPLING_FREQUENCY,
+					     &odr);
+			if (rc != 0) {
+				printk("Failed to set odr: %d\n", rc);
+				return;
+			}
+			printk("Sampling at %u Hz\n", odr.val1);
+		}
+
+		rc = sensor_trigger_set(sensor, &trig, trigger_handler);
+		if (rc != 0) {
+			printk("Failed to set trigger: %d\n", rc);
+			return;
+		}
+
+		printk("Waiting for triggers\n");
+		while (true) {
+			k_sleep(K_MSEC(2000));
+		}
+	}
+#else /* CONFIG_LIS2DH_TRIGGER */
+	printk("Polling at 0.5 Hz\n");
+	while (true) {
+		fetch_and_display(shell, sensor);
+		k_sleep(K_MSEC(100));
+	}
+#endif /* CONFIG_LIS2DH_TRIGGER */
+}
+
+
+
+
 static void ble_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
 {
 	simulate_ble = (value == BT_GATT_CCC_NOTIFY) ? 1 : 0;
